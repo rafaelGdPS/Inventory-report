66f950e21474e27fe82f6ef4293bda8b2a0e6ead
Rafael <rafaelgermanio@gmail.com>
2024-06-13 16:10:59 -0300

    test: Implementando testes para os requisitos 01 e 02

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,28 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    id = "5"
+    product_name = "Iogurt"
+    company_name = "Test&&CIA"
+    manufacturing_date = "2024/06/13"
+    expiration_date = "24/07/13"
+    serial_number = "555724"
+    storage_instructions = "Armazenar sempre em lugares refrigerados"
+    product = Product(
+        id,
+        product_name,
+        company_name,
+        manufacturing_date,
+        expiration_date,
+        serial_number,
+        storage_instructions,
+    )
+
+    assert product.id == id
+    assert product.product_name == product_name
+    assert product.company_name == company_name
+    assert product.manufacturing_date == manufacturing_date
+    assert product.expiration_date == expiration_date
+    assert product.serial_number == serial_number
+    assert product.storage_instructions == storage_instructions
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,29 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    id = "5"
+    company_name = "Test&&CIA"
+    product_name = "Iogurt"
+    manufacturing_date = "2024/06/13"
+    expiration_date = "24/07/13"
+    serial_number = "555724"
+    storage_instructions = "Armazenar sempre em lugares refrigerados"
+    product = Product(
+        id,
+        product_name,
+        company_name,
+        manufacturing_date,
+        expiration_date,
+        serial_number,
+        storage_instructions,
+    )
+    assert f"The product {id} - {product_name} " in product.__str__()
+    assert f"with serial number {serial_number} " in product.__str__()
+    assert f"manufactured on {manufacturing_date} " in product.__str__()
+    assert f"by the company {company_name} " in product.__str__()
+    assert f"valid until {expiration_date} " in product.__str__()
+    assert (
+        "must be stored according to the following instructions: "
+        f"{storage_instructions}." in product.__str__()
+        )
