3a9a2cda60096336976514cb4d0fa7041cf33d7e
Rafael <rafaelgermanio@gmail.com>
2024-06-13 17:59:31 -0300

    feat: implementando requisito 5

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,15 @@
+from typing import Optional
+
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[list[Product]] = None) -> None:
+        self.__data = [] if not data else data
+
+    @property
+    def data(self):
+        return self.__data
+
+    def add_data(self, data: list[Product]):
+        self.data.extend(data)
